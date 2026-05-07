9e33c23090b7b714061e0c1da69bc975d1ded153
Nick Driscoll <nick@lunarg.com>
2026-05-07 09:05:38 -0400

    simplify + bugfig

--- framework/decode/vulkan_replay_frame_loop_consumer.h
+++ framework/decode/vulkan_replay_frame_loop_consumer.h
@@ -161,7 +161,6 @@ class VulkanReplayFrameLoopConsumer : public VulkanReplayConsumer
 
   private:
     graphics::FrameLoopInfo&             frame_loop_info_;
-    std::unordered_set<VkDescriptorPool> active_descriptor_pools_;
 };
 
 GFXRECON_END_NAMESPACE(decode)
